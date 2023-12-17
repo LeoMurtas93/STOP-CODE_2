ba625755718be60cf3c8b34e54cd35771edf8cae
Alessandro Murtas <alessandromurtas93@gmail.com>
2023-12-17 14:25:03 +0100

    ToSearchBar

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    JSON_SERVER_BASE_URL: "http://localhost:3000",
+    JSON_URL_API_GAMES: "https://api.rawg.io/api/platforms?key=54e9f98ef79b43179c3d10f0d6241706"
+};
